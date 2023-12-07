85c2c70bd9689b96ef46305f47035ba444ad95e3
irshad500 <mohammedirshadkp500@gmail.com>
2023-12-07 13:24:50 +0530

    ui updated2

--- lib/features/home/screens/home_screen.dart
+++ lib/features/home/screens/home_screen.dart
@@ -9,24 +9,49 @@ import 'package:ifsc_get_bank_details/models/bank_details_model.dart';
 import 'package:responsive_framework/responsive_framework.dart';
 import 'package:http/http.dart' as http;
 
+String? errorMessage;
+bool isLoading = false;
+
 class HomeScreen extends ConsumerStatefulWidget {
   const HomeScreen({super.key});
-
   @override
   ConsumerState<ConsumerStatefulWidget> createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends ConsumerState<HomeScreen> {
   TextEditingController ifsccontroller = TextEditingController();
-  Future<void> fetchData(String ifscCode) async {
-    final url = 'http://ifsc.rizad.me/?ifsc=$ifscCode';
 
-    final response = await http.get(Uri.parse(url));
-    if (response.statusCode == 200) {
-      final responseData = json.decode(response.body);
+  Future<void> fetchData(String ifscCode) async {
+    try {
+      if (ifscCode.isEmpty || ifscCode.length != 11) {
+        setState(() {
+          errorMessage = 'Invalid IFSC code';
+          bankDetails = null; // Reset bankDetails if code is invalid
+        });
+        return; // Exit the function early if IFSC code is invalid
+      }
+
+      final url = 'http://ifsc.rizad.me/?ifsc=$ifscCode';
+      final response = await http.get(Uri.parse(url));
+
+      if (response.statusCode == 200) {
+        final responseData = json.decode(response.body);
+        setState(() {
+          bankDetails = BankDetails.fromMap(responseData);
+          errorMessage = null; // Reset error message if successful
+        });
+      } else {
+        setState(() {
+          errorMessage = 'Failed to fetch data: ${response.statusCode}';
+        });
+      }
+    } catch (e) {
       setState(() {
-        bankDetails = BankDetails.fromMap(responseData);
+        errorMessage = 'Error fetching data: $e';
       });
+      print('Error fetching data: $e');
+    } finally {
+      isLoading = false;
     }
   }
 
@@ -58,27 +83,27 @@ class _HomeScreenState extends ConsumerState<HomeScreen> {
                       child: Padding(
                         padding: const EdgeInsets.all(defaultPadding),
                         child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.center,
                           children: [
-                            const Text(
-                              "Enter IFSC code",
-                              style:
-                                  TextStyle(fontSize: 16, color: Colors.white),
-                            ),
-                            const SizedBox(
-                              height: 5,
-                            ),
                             TextFormField(
-                              style: TextStyle(color: Colors.white),
                               controller: ifsccontroller,
                               decoration: InputDecoration(
-                                fillColor: Colors.black,
-                                border: OutlineInputBorder(
-                                  borderSide: BorderSide(
-                                      color: Colors.white,
-                                      style: BorderStyle.solid,
-                                      width: 2),
+                                enabledBorder: OutlineInputBorder(
+                                  borderSide: const BorderSide(
+                                      color: Colors.transparent),
+                                  borderRadius: BorderRadius.circular(5.5),
                                 ),
+                                focusedBorder: OutlineInputBorder(
+                                  borderSide: const BorderSide(
+                                      color: Colors.transparent),
+                                  borderRadius: BorderRadius.circular(5.5),
+                                ),
+                                prefixIcon: const Icon(Icons.account_balance,
+                                    color: Colors.blue),
+                                hintText: "Enter your IFSC CODE",
+                                hintStyle: TextStyle(color: Colors.blue),
+                                filled: true,
+                                fillColor: Colors.blue[50],
                               ),
                             )
                           ],
@@ -94,10 +119,13 @@ class _HomeScreenState extends ConsumerState<HomeScreen> {
                         ElevatedButton(
                           style: ElevatedButton.styleFrom(
                             backgroundColor: Colors.blueAccent,
-                            fixedSize: Size(130, 50),
+                            fixedSize: const Size(130, 50),
                           ),
                           onPressed: () {
-                            fetchData(ifsccontroller.text);
+                            setState(() {
+                              fetchData(ifsccontroller.text);
+                              isLoading = true;
+                            });
                           },
                           child: const Text(
                             "Search",
@@ -109,68 +137,90 @@ class _HomeScreenState extends ConsumerState<HomeScreen> {
                     const SizedBox(
                       height: 10,
                     ),
-                    Container(
-                      height: 650,
-                      color: Palette.scaffoldBackgorundColor,
-                      child: Card(
-                          color: Palette.scaffoldBackgorundColor,
-                          child: bankDetails != null
-                              ? ListView(
-                                  physics: const ScrollPhysics(
-                                    parent: BouncingScrollPhysics(
-                                        decelerationRate:
-                                            ScrollDecelerationRate.normal),
-                                  ),
-                                  children: [
-                                    DetailsTile(
-                                        data: bankDetails!.bank, title: "bank"),
-                                    DetailsTile(
-                                        data: bankDetails!.branch,
-                                        title: "branch"),
-                                    DetailsTile(
-                                        data: bankDetails!.address,
-                                        title: "address"),
-                                    DetailsTile(
-                                        data: bankDetails!.state,
-                                        title: "state"),
-                                    DetailsTile(
-                                        data: bankDetails!.city, title: "city"),
-                                    DetailsTile(
-                                        data: bankDetails!.centre,
-                                        title: "centre"),
-                                    DetailsTile(
-                                        data: bankDetails!.contact,
-                                        title: "contact"),
-                                    DetailsTile(
-                                        data: bankDetails!.district,
-                                        title: "district"),
-                                    DetailsTile(
-                                        data: bankDetails!.ifsc, title: "ifsc"),
-                                    DetailsTile(
-                                        data: bankDetails!.iso3166,
-                                        title: "iso3166"),
-                                    DetailsTile(
-                                        data: bankDetails!.micr, title: "micr"),
-                                    DetailsTile(
-                                        data: bankDetails!.imps.toString(),
-                                        title: "imps"),
-                                    DetailsTile(
-                                        data: bankDetails!.neft.toString(),
-                                        title: "neft"),
-                                    DetailsTile(
-                                        data: bankDetails!.rtgs.toString(),
-                                        title: "rtgs"),
-                                    DetailsTile(
-                                        data: bankDetails!.upi.toString(),
-                                        title: "upi"),
-                                    DetailsTile(
-                                        data: bankDetails!.swift,
-                                        title: "swift"),
-                                  ],
-                                )
-                              : const Center(
-                                  child: Text('Pls Enter a IFSC Code'))),
-                    )
+                    isLoading == true
+                        ? const SizedBox(
+                            height: 650,
+                            child: Center(
+                              child: CircularProgressIndicator(),
+                            ),
+                          )
+                        : Container(
+                            height: 650,
+                            color: Palette.scaffoldBackgorundColor,
+                            child: Card(
+                              elevation: 0,
+                              color: Palette.scaffoldBackgorundColor,
+                              child: bankDetails != null
+                                  ? ListView(
+                                      physics: const ScrollPhysics(
+                                        parent: BouncingScrollPhysics(
+                                            decelerationRate:
+                                                ScrollDecelerationRate.normal),
+                                      ),
+                                      children: [
+                                        DetailsTile(
+                                            data: bankDetails!.bank,
+                                            title: "bank"),
+                                        DetailsTile(
+                                            data: bankDetails!.branch,
+                                            title: "branch"),
+                                        DetailsTile(
+                                            data: bankDetails!.address,
+                                            title: "address"),
+                                        DetailsTile(
+                                            data: bankDetails!.state,
+                                            title: "state"),
+                                        DetailsTile(
+                                            data: bankDetails!.city,
+                                            title: "city"),
+                                        DetailsTile(
+                                            data: bankDetails!.centre,
+                                            title: "centre"),
+                                        DetailsTile(
+                                            data: bankDetails!.contact,
+                                            title: "contact"),
+                                        DetailsTile(
+                                            data: bankDetails!.district,
+                                            title: "district"),
+                                        DetailsTile(
+                                            data: bankDetails!.ifsc,
+                                            title: "ifsc"),
+                                        DetailsTile(
+                                            data: bankDetails!.iso3166,
+                                            title: "iso3166"),
+                                        DetailsTile(
+                                            data: bankDetails!.micr,
+                                            title: "micr"),
+                                        DetailsTile(
+                                            data: bankDetails!.imps.toString(),
+                                            title: "imps"),
+                                        DetailsTile(
+                                            data: bankDetails!.neft.toString(),
+                                            title: "neft"),
+                                        DetailsTile(
+                                            data: bankDetails!.rtgs.toString(),
+                                            title: "rtgs"),
+                                        DetailsTile(
+                                            data: bankDetails!.upi.toString(),
+                                            title: "upi"),
+                                        DetailsTile(
+                                            data: bankDetails!.swift,
+                                            title: "swift"),
+                                      ],
+                                    )
+                                  : errorMessage != null
+                                      ? Padding(
+                                          padding: const EdgeInsets.all(8.0),
+                                          child: Text(
+                                            errorMessage!,
+                                            style: TextStyle(
+                                                color: Colors
+                                                    .red), // Style the error message here
+                                          ),
+                                        )
+                                      : SizedBox.shrink(),
+                            ),
+                          )
                   ],
                 ),
               ),
