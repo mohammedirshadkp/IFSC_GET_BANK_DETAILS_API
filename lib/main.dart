9dfa6c862550f70ea8ee942b4a46d2bc34a3f6b0
irshad500 <mohammedirshadkp500@gmail.com>
2023-12-16 11:53:48 +0530

    ui updated2

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,28 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:ifsc_get_bank_details/features/home/screens/home_screen.dart';
-import 'package:responsive_framework/breakpoint.dart';
-import 'package:responsive_framework/responsive_breakpoints.dart';
-
-void main() {
-  runApp(
-    const ProviderScope(
-      child: MyApp(),
-    ),
-  );
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      builder: (context, child) => ResponsiveBreakpoints(breakpoints: const [
-        Breakpoint(start: 0, end: 450, name: MOBILE),
-      ], child: child!),
-      home: const HomeScreen(),
-    );
-  }
-}
+// import 'package:flutter/material.dart';
+// import 'package:flutter_riverpod/flutter_riverpod.dart';
+// import 'package:ifsc_get_bank_details/features/home/screens/home_screen.dart';
+// import 'package:responsive_framework/breakpoint.dart';
+// import 'package:responsive_framework/responsive_breakpoints.dart';
+//
+// void main() {
+//   runApp(
+//     const ProviderScope(
+//       child: MyApp(),
+//     ),
+//   );
+// }
+//
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       builder: (context, child) => ResponsiveBreakpoints(breakpoints: const [
+//         Breakpoint(start: 0, end: 450, name: MOBILE),
+//       ], child: child!),
+//       home: const HomeScreen(),
+//     );
+//   }
+// }
